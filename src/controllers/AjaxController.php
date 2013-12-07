b85de50c93cc31d9bd67937bccf9e7079a413a99
paulboco <paulboco@cableone.net>
2013-12-07 17:05:40 -0600

    Convert tab to spaces.

--- src/controllers/AjaxController.php
+++ src/controllers/AjaxController.php
@@ -8,23 +8,23 @@ use Laravelerator\Alg\Template;
 
 class AjaxController extends BaseController {
 
-	/**
-	 * Ajax request for write path display
-	 */
-	public function path()
-	{
-		return View::make('laravelerator::controllers.ajax.write_path_display');
-	}
+    /**
+     * Ajax request for write path display
+     */
+    public function path()
+    {
+        return View::make('laravelerator::controllers.ajax.write_path_display');
+    }
 
-	/**
-	 * Ajax request for selected template description
-	 */
-	public function template()
-	{
-    	$data = Template::getConfig();
+    /**
+     * Ajax request for selected template description
+     */
+    public function template()
+    {
+        $data = Template::getConfig();
 
-		return View::make('laravelerator::controllers.ajax.template_description', $data);
-	}
+        return View::make('laravelerator::controllers.ajax.template_description', $data);
+    }
 
 
 }
