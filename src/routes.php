45d8f357c6d4e683b3fe2ba635836cee20ec3108
paulboco <paulboco@cableone.net>
2013-12-07 22:18:39 -0600

    Group routes.

--- src/routes.php
+++ src/routes.php
@@ -1,22 +1,43 @@
 <?php
 
-Route::group(array('prefix' => 'laravelerator'), function()
+Route::group(['prefix' => 'laravelerator'], function()
 {
+	/*
+	|--------------------------------------------------------------------------
+	| Ajax
+	|--------------------------------------------------------------------------
+	*/
+
+	Route::group(['before' => 'ajax|csrf'], function()
+	{
+		Route::get('ajax/path', 'Laravelerator\Laravelerator\AjaxController@path');
+		Route::get('ajax/template', 'Laravelerator\Laravelerator\AjaxController@template');
+	});
+
+	/*
+	|--------------------------------------------------------------------------
+	| Generate
+	|--------------------------------------------------------------------------
+	*/
+
 	Route::get('generate/create', 'Laravelerator\Laravelerator\GenerateController@create');
 	Route::post('generate/show', 'Laravelerator\Laravelerator\GenerateController@show');
 
-	Route::get('ajax/path', [
-		'before' => 'ajax|csrf',
-		'uses' => 'Laravelerator\Laravelerator\AjaxController@path'
-	]);
-
-	Route::get('ajax/template', [
-		'before' => 'ajax|csrf',
-		'uses' => 'Laravelerator\Laravelerator\AjaxController@template'
-	]);
+	/*
+	|--------------------------------------------------------------------------
+	| Generate
+	|--------------------------------------------------------------------------
+	*/
 
 	Route::get('/', 'Laravelerator\Laravelerator\PagesController@home');
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| Ajax
+|--------------------------------------------------------------------------
+*/
 
 Route::filter('ajax', function()
 {
