44a5cd9217903ba29256a181f82e3806d8f3e419
paulboco <paulboco@cableone.net>
2013-12-13 19:06:22 -0600

    Open accordion on snippet in development.

--- src/views/controllers/generate/partials/generated_files.blade.php
+++ src/views/controllers/generate/partials/generated_files.blade.php
@@ -4,11 +4,12 @@
             <h3>Files to be created</h3>
             <div class="panel-group" id="accordion">
                 @foreach ($files as $path => $contents)
+                    <?php $in = $path == '/home/pbc/git/laravelerator/laravel-alg.dev/app/views/problems/_inputs.blade.php' ? ' in' : '' ?>
                     <div class="panel panel-default">
                         <div class="panel-heading directories" data-toggle="collapse" data-parent="#accordion" href="#collapse-{{ md5($path) }}">
                             {{ mute_base_path($path) }}
                         </div>
-                        <div id="collapse-{{ md5($path) }}" class="panel-collapse collapse">
+                        <div id="collapse-{{ md5($path) }}" class="panel-collapse collapse{{ $in }}">
                             <div class="panel-body">
                                 <pre class="prettyprint linenums">{{ htmlentities($contents) }}</pre>
                             </div>
