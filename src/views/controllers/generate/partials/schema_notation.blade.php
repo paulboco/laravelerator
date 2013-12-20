f4d8f6cf7c87fef32cd154918037915ec75f9286
paulboco <paulboco@cableone.net>
2013-12-19 19:59:56 -0600

    Reformat heading.

--- src/views/controllers/generate/partials/schema_notation.blade.php
+++ src/views/controllers/generate/partials/schema_notation.blade.php
@@ -2,7 +2,7 @@
 	<h4>Schema Notation</h4>
 	<ul class="list-group">
 		@foreach ($schemaNotation as $notation => $method)
-			<li class="list-group-item" data-toggle="popover" data-placement="left" data-original-title="Laravel equivalent:" data-content="{{{ $method }}}">
+			<li class="list-group-item" data-toggle="popover" data-placement="left" data-original-title="Laravel Equivalent" data-content="{{{ $method }}}">
 				<span>{{ $notation }}</span>
 			</li>
 		@endforeach
